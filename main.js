e5fc1e2638821435b504861fe164cedea09a815e
sangya2001 <sangyasherpa2058@gmail.com>
2020-08-18 10:49:55 +0545

    Proof of work is also added

--- main.js
+++ main.js
@@ -7,25 +7,38 @@ class Block {
         this.data = data;
         this.previousHash = previousHash;
         this.hash = this.calculateHash();
+        this.nonce = 0;
     }
 
     calculateHash = () => {
-        return SHA256(this.index + this.timestamp + JSON.stringify(this.data) + this.previousHash).toString();
+        return SHA256(this.index + this.timestamp + JSON.stringify(this.data) + this.previousHash + this.nonce).toString();
+    }
+
+    mineBlock(difficulty){
+        while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")){
+            this.nonce++;
+            this.hash = this.calculateHash();
+        }
+
+        console.log("Block mined:", this.hash)
     }
 }
 
 class Blockchain {
     constructor() {
         this.chain = [new Block(0, Date.now(), "Genesis Block", "0")];
+        this.difficulty = 4;
     }
 
     getLatestBlock = () => {
         return this.chain[this.chain.length - 1];
     }
 
-    addNewBlock = (data) =>{
+    addNewBlock = (newBlock) =>{
         const latestBlock = this.getLatestBlock();
-        this.chain.push(new Block(latestBlock.index + 1, Date.now(), data, latestBlock.hash));
+        newBlock.previousHash = latestBlock.hash;
+        newBlock.mineBlock(this.difficulty);
+        this.chain.push(newBlock);
     }
 
     isChainValid = () => {
@@ -44,14 +57,17 @@ class Blockchain {
 
 const coin = new Blockchain();
 
-coin.addNewBlock({amount: "$100"});
-coin.addNewBlock({amount: "$200"});
+console.log("Mining block 1...")
+coin.addNewBlock(new Block(coin.getLatestBlock().index + 1, Date.now(), {amount: "$100"}));
 
-console.log(coin.isChainValid())
+console.log("Mining block 2...")
+coin.addNewBlock(new Block(coin.getLatestBlock().index + 1, Date.now(), {amount: "$200"}));
+
+// console.log(coin.isChainValid())
 
 // tampering the chain
-coin.chain[1].data = "tampered data";
-coin.chain[1].hash = coin.chain[1].calculateHash();
+// coin.chain[1].data = "tampered data";
+// coin.chain[1].hash = coin.chain[1].calculateHash();
 
 console.log(coin.chain)
-console.log(coin.isChainValid())
+// console.log(coin.isChainValid())
