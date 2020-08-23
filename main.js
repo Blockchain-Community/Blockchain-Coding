8783a11c8d2c6487c2488fe7d03b62e4991f3b60
sangya2001 <sangyasherpa2058@gmail.com>
2020-08-23 12:13:21 +0545

    Part 1 finished

--- main.js
+++ main.js
@@ -1,73 +1,77 @@
-const SHA256 = require('crypto-js/sha256');
+const SHA256 = require('crypto-js/sha256')
 
+// Block Structure
 class Block {
-    constructor(index, timestamp, data, previousHash) {
+    constructor(index, timestamp, data, previousHash = '') {
         this.index = index;
         this.timestamp = timestamp;
         this.data = data;
-        this.previousHash = previousHash;
-        this.hash = this.calculateHash();
-        this.nonce = 0;
+        this.previousHash = previousHash
+        this.hash = this.calculateHash()
     }
 
-    calculateHash = () => {
-        return SHA256(this.index + this.timestamp + JSON.stringify(this.data) + this.previousHash + this.nonce).toString();
-    }
-
-    mineBlock(difficulty){
-        while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")){
-            this.nonce++;
-            this.hash = this.calculateHash();
-        }
-
-        console.log("Block mined:", this.hash)
+    // In calculating the hash SHA256 is used
+    calculateHash() {
+        //for the hash index, timestamp, data, previous hash is used
+        return SHA256(this.index +
+            this.timestamp +
+            this.previousHash +
+            JSON.stringify(this.data)).toString()
     }
 }
 
+// Simple Blockchain Structure
 class Blockchain {
     constructor() {
-        this.chain = [new Block(0, Date.now(), "Genesis Block", "0")];
-        this.difficulty = 4;
+        //rather than staring empty blockchain a genesis block is created
+        this.chain = [this.createGenesisBlock()];
     }
 
-    getLatestBlock = () => {
-        return this.chain[this.chain.length - 1];
+    // method for initiating genesis block using block inheritance feature
+    createGenesisBlock() {
+        return new Block(0, Date.now(), "Genesis Block", "0")
     }
 
-    addNewBlock = (newBlock) =>{
-        const latestBlock = this.getLatestBlock();
-        newBlock.previousHash = latestBlock.hash;
-        newBlock.mineBlock(this.difficulty);
-        this.chain.push(newBlock);
+    // method to get the latest block created
+    getLatestBlock() {
+        return this.chain[this.chain.length - 1]
     }
 
-    isChainValid = () => {
-        for(let i = 1; i < this.chain.length; i++){
+    // method to add new block to the chain
+    addBlock(newBlock) {
+        newBlock.previousHash = this.getLatestBlock().hash;
+        newBlock.hash = newBlock.calculateHash();
+        this.chain.push(newBlock)
+    }
+
+    // method to check if the chain is valid or not
+    isChainValid() {
+        for (let i = 1; i < this.chain.length; i++) {
             const previousBlock = this.chain[i - 1];
             const currentBlock = this.chain[i];
 
-            if(currentBlock.hash !== currentBlock.calculateHash()) return false;
+            // if the input are same then the hash generated is always same
+            if (currentBlock.hash !== currentBlock.calculateHash()) return false;
 
-            if(currentBlock.previousHash !== previousBlock.hash) return false;
+            // check if linked hash is correct or not
+            if (currentBlock.previousHash !== previousBlock.hash) return false;
         }
 
         return true;
     }
 }
 
-const coin = new Blockchain();
+// intiating nee chain - OOP
+let coin = new Blockchain();
 
-console.log("Mining block 1...")
-coin.addNewBlock(new Block(coin.getLatestBlock().index + 1, Date.now(), {amount: "$100"}));
+// manually adding new blocks
+coin.addBlock(new Block(coin.getLatestBlock().index + 1, Date.now(), { amount: "$4" }))
+coin.addBlock(new Block(coin.getLatestBlock().index + 1, Date.now(), { amount: "$8" }))
 
-console.log("Mining block 2...")
-coin.addNewBlock(new Block(coin.getLatestBlock().index + 1, Date.now(), {amount: "$200"}));
+// tampering the block and check if it detetcs the error or not
+console.log(coin.isChainValid())
+coin.chain[1].data = { amount: "$10" };
+coin.chain[1].hash = coin.chain[1].calculateHash();
+console.log(coin.isChainValid())
 
-// console.log(coin.isChainValid())
-
-// tampering the chain
-// coin.chain[1].data = "tampered data";
-// coin.chain[1].hash = coin.chain[1].calculateHash();
-
-console.log(coin.chain)
-// console.log(coin.isChainValid())
+console.log(JSON.stringify(coin, null, 4))
